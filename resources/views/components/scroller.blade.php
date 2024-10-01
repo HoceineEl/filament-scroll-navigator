335478645138378907dcbaf80caee50548c4a14e
hoceineel <hoceineelidrissi@gmail.com>
2024-10-01 07:54:11 +0100

    choose color

--- resources/views/components/scroller.blade.php
+++ resources/views/components/scroller.blade.php
@@ -5,17 +5,10 @@
         x-transition:enter-end="opacity-100 transform translate-y-0" x-transition:leave="transition ease-in duration-300"
         x-transition:leave-start="opacity-100 transform translate-y-0"
         x-transition:leave-end="opacity-0 transform translate-y-2" @click="scrollToPosition"
-        class="bg-{{ $color }}-600 hover:bg-{{ $color }}-700 text-white font-bold p-3 rounded-full shadow-lg transition-all duration-300 ease-in-out relative overflow-hidden group">
-        <svg xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 transition-transform duration-300 ease-in-out relative z-10"
+        class="bg-{{ $color }}-600 hover:bg-{{ $color }}-700 text-white font-bold p-3 rounded-full shadow-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 transition-transform duration-300 ease-in-out"
             :class="{ 'rotate-180': isAtBottom }" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
         </svg>
-        <div
-            class="absolute inset-0 bg-gradient-to-br from-{{ $color }}-400 to-{{ $color }}-800 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
-        </div>
-        <div
-            class="absolute -inset-1 bg-gradient-to-r from-{{ $color }}-600 via-{{ $color }}-400 to-{{ $color }}-600 rounded-full blur opacity-30 group-hover:opacity-100 transition duration-1000 group-hover:duration-200">
-        </div>
     </button>
 </div>
--- src/FilamentScrollNavigatorPlugin.php
+++ src/FilamentScrollNavigatorPlugin.php
@@ -6,23 +6,36 @@ use Closure;
 use Filament\Contracts\Plugin;
 use Filament\Facades\Filament;
 use Filament\Panel;
+use Filament\Support\Colors\Color;
 use Filament\View\PanelsRenderHook;
 use HoceineEl\FilamentScrollNavigator\Components\ScrollNavigator;
 
 class FilamentScrollNavigatorPlugin implements Plugin
 {
-    public string $color = 'primary';
+    public string | Color | Closure $color = 'primary';
 
     public function getId(): string
     {
         return 'filament-scroll-navigator';
     }
 
+    public function color(Color|string|Closure $color): static
+    {
+        $this->color = $color instanceof Closure ? $color() : $color;
+        return $this;
+    }
+
     public function register(Panel $panel): void
     {
         Filament::registerRenderHook(
             PanelsRenderHook::BODY_END,
-            fn() => view('filament-scroll-navigator::components.scroller', ['color' => $this->color]),
+            function () {
+                $colorValue = $this->color instanceof Color
+                    ? $this->color[500]
+                    : $this->color;
+
+                return view('filament-scroll-navigator::components.scroller', ['color' => $colorValue]);
+            },
         );
     }
 
@@ -39,12 +52,5 @@ class FilamentScrollNavigatorPlugin implements Plugin
         $plugin = filament(app(static::class)->getId());
 
         return $plugin;
-    }
-
-    public function color(string | Closure  $color): static
-    {
-
-        $this->color = $color instanceof Closure ? $color() : $color;
-        return $this;
     }
 }
